acbfb936c99cc5604776f53b3b5a2449c4a891da
Nitesh Choudhary <nitesh.choudhary.tech@gmail.com>
2026-05-18 09:28:23 +0530

    Cors Configuration added

---
+++ src/main/java/com/nextgen/configuration/CorsConfiguration.java
@@ -0,0 +1,24 @@
+package com.nextgen.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+
+                registry.addMapping("/**")
+                        .allowedOrigins("https://vercel.com/choudhary-ns-projects/nextgen-frontend")
+                        .allowedMethods("*");
+            }
+        };
+    }
+}
+
